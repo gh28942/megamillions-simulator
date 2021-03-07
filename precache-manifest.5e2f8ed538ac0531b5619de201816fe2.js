14d670dc519acfefc4b624ae124ddc297b106bab
gh28942 <GerH@example.com>
2021-03-07 02:36:07 +0100

    Updates

---
+++ precache-manifest.5e2f8ed538ac0531b5619de201816fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54a534f77a128c72ec2d1c2f36e635c",
+    "url": "/megamillions-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/megamillions-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "a5b1ae5337ebf178c976",
+    "url": "/megamillions-simulator/static/css/main.dccd57aa.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/megamillions-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/megamillions-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b1ae5337ebf178c976",
+    "url": "/megamillions-simulator/static/js/main.dba63ddf.chunk.js"
+  },
+  {
+    "revision": "76e05cca07336b1d3809",
+    "url": "/megamillions-simulator/static/js/runtime-main.f666b12c.js"
+  }
+]);
